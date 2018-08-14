5c0688b8da7383a3ae886335fdad7f1a95eb7c34
David <davidlago104@gmail.com>
2018-08-14 14:55:28 -0400

    done

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,16 +1,43 @@
 class Gym
- ALL = []
+
+ @@all = []
 
   attr_reader :name
 
   def initialize(name)
     @name = name
-    ALL << self
+    @@all << self
 
   end
 
   def self.all
-    ALL
+    @@all
   end
 
-end
+  def memberships
+    Membership.all.select do |mems|
+      mems.gym == self
+    end
+  end
+
+  def lifters
+    memberships.map do |membership|
+      membership.lifter
+    end
+  end
+
+  def lifter_names
+    lifters.map do |lifter|
+      lifter.name
+    end
+  end
+
+  def lift_totals
+    lift_totals = lifters.map do |lifter|
+      lifter.lift_total
+    end
+    binding.pry
+    lift_totals.reduce(:+)
+  end
+
+end #end of class
